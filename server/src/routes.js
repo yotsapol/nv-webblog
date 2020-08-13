fc804124436851c1f71f225db43224c803aef270
Yotsapol <yotsapolp61@nu.ac.th>
2020-08-13 10:04:13 +0700

    folder prepared

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,14 @@
 const UserController = require('./controllers/UserController') 
 const UserAuthenController = require('./controllers/UserAuthenController')
-const isAuthenController = require('./authen/isAuthenController') 
+const isAuthenController = require('./authen/isAuthenController')
+const BlogController = require('./controllers/BlogController') 
 
 module.exports = (app) => {   
     /* RESFUL Api for users management */   
     // create user   
     app.post('/user',UserController.create   
     ) 
+
     //login
     app.post('/login',UserAuthenController.login
     )
@@ -25,5 +27,26 @@ module.exports = (app) => {
  
     // get all user   
     app.get('/users',isAuthenController, UserController.index
+    )
+
+    // blog route  
+    // create blog  
+    app.post('/blog',    BlogController.create  
+    ) 
+ 
+    // edit blog, suspend, active  
+    app.put('/blog/:blogId',    BlogController.put  
+    ) 
+ 
+    // delete blog  
+    app.delete('/blog/:blogId',    BlogController.remove  
+    ) 
+ 
+    // get blog by id  
+    app.get('/blog/:blogId',    BlogController.show  
+    ) 
+ 
+    // get all blog  
+    app.get('/blogs',    BlogController.index     
     )
 } 
